dc5cc1d0821a21233f6528af75243e3ac623593a
Sean Tyler Atwell <sta0003@auburn.edu>
2015-05-17 23:15:19 -0500

    added the existing project

---
+++ HitReportsWebpage/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+
+namespace HitReportsWebpage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a request for the URL. 
+            WebRequest request = WebRequest.Create("http://localhost/InventoryManager/ReportExport/ShippingReport?comprehensive=true");
+            request.Timeout = 2000000000;
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+
+
+            // Create a request for the URL. 
+            request = WebRequest.Create("http://localhost/InventoryManager/ReportExport/TotalReport");
+            request.Timeout = 2000000000;
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            reader = new StreamReader(dataStream);
+            // Read the content.
+            responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+        }
+    }
+}
